57f8ec1c846fbf58d6da8212ac14ce2f76aaf2b1
shibata-kenshin <shibata@enaspiration.com>
2023-02-24 00:32:16 +0900

    complete

---
+++ section3/const.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14
+
+const (
+	Username = "test_user"
+	Password = "test=pass"
+)
+
+// constは型を指定しない
+
+func main() {
+	fmt.Println(Pi, Username, Password)
+}

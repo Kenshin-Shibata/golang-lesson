4b4453c68320754eb60491d62b0a803371f86999
shibata-kenshin <shibata@enaspiration.com>
2023-02-25 04:00:35 +0900

    complete

---
+++ section3/inteter.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var (
+	// 	u8  uint8     = 255
+	// 	i8  int8      = 127
+	// 	f32 float32   = 0.2
+	// 	c64 complex64 = -5 + 12i
+	// )
+	// fmt.Println(u8, i8, f32, c64)
+	fmt.Println("1 + 1 = ", 1+1)
+	fmt.Println("10 - 1 = ", 10-1)
+	// 二進数 シフト
+	fmt.Println(1 << 0)
+	fmt.Println(1 << 1)
+	fmt.Println(1 << 2)
+}
